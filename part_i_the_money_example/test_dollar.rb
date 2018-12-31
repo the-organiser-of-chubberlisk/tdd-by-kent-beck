02409d8d5bc74da6490e3ea418027ebbeb975bf0
Wen Ting Wang <chubberlisk@outlook.com>
2018-12-31 17:43:43 +0000

    part_i: return new dollar object instead of value (#1)

--- part_i_the_money_example/test_dollar.rb
+++ part_i_the_money_example/test_dollar.rb
@@ -4,7 +4,9 @@ require_relative './dollar'
 class TestDollar < Minitest::Test
   def test_multiplication
     five = Dollar.new(5)
-    five.times(2)
-    assert_equal 10, five.amount
+    product = five.times(2)
+    assert_equal 10, product.amount
+    product = five.times(3)
+    assert_equal 15, product.amount
   end
 end
