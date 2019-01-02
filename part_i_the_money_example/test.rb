af5b246bb98f16093c3a2f549dfecbaaf4acd350
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-02 17:32:55 +0000

    part_i: add tests for franc (#1)

--- part_i_the_money_example/test.rb
+++ part_i_the_money_example/test.rb
@@ -1,5 +1,6 @@
 require 'minitest/autorun'
 require_relative './dollar'
+require_relative './franc'
 
 class Test < Minitest::Test
   def test_multiplication
@@ -11,5 +12,13 @@ class Test < Minitest::Test
   def test_equality
     assert Dollar.new(5).equal?(Dollar.new(5))
     refute Dollar.new(5).equal?(Dollar.new(6))
+    assert Franc.new(5).equal?(Franc.new(5))
+    refute Franc.new(5).equal?(Franc.new(6))
+  end
+
+  def test_franc_multiplication
+    five = Franc.new(5)
+    assert_equal Franc.new(10), five.times(2)
+    assert_equal Franc.new(15), five.times(3)
   end
 end
