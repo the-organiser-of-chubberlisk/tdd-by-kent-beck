3a76ee7efddc3d3bbf9d47f4cbfc03e31565ef0a
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-02 17:40:58 +0000

    part_i: ensure same class when checking equality (#1)

--- part_i_the_money_example/money.rb
+++ part_i_the_money_example/money.rb
@@ -2,10 +2,10 @@ class Money
   attr_reader :amount
 
   def equal?(money)
-    self.amount == money.amount
+    self.amount == money.amount && self.class == money.class
   end
 
   def ==(money)
-    self.amount == money.amount
+    self.amount == money.amount && self.class == money.class
   end
 end
