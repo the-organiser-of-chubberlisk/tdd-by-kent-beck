7d93ec67ed1a4e84e30df58c1ac7adba8b927598
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-02 16:38:00 +0000

    part_i: improve readability of multiplication test (#1)

--- part_i_the_money_example/test_dollar.rb
+++ part_i_the_money_example/test_dollar.rb
@@ -4,10 +4,8 @@ require_relative './dollar'
 class TestDollar < Minitest::Test
   def test_multiplication
     five = Dollar.new(5)
-    product = five.times(2)
-    assert_equal 10, product.amount
-    product = five.times(3)
-    assert_equal 15, product.amount
+    assert_equal Dollar.new(10), five.times(2)
+    assert_equal Dollar.new(15), five.times(3)
   end
 
   def test_equality
