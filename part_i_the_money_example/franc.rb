417a6b1d32a93b7932959f9782f599819d74d945
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-02 17:32:13 +0000

    part_i: create franc class (#1)

---
+++ part_i_the_money_example/franc.rb
@@ -0,0 +1,11 @@
+require_relative './money'
+
+class Franc < Money
+  def initialize(amount)
+    @amount = amount
+  end
+  
+  def times(multiplier)
+    Franc.new(@amount * multiplier)
+  end
+end
