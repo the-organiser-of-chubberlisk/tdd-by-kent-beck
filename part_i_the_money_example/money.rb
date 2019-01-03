27b7afd45b762026a4a4434bd92c36ba8dbb7e62
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-03 17:12:06 +0000

    part_i: add methods to create dollar and franc in money superclass (#1)

--- part_i_the_money_example/money.rb
+++ part_i_the_money_example/money.rb
@@ -1,6 +1,14 @@
 class Money
   attr_reader :amount
 
+  def self.dollar(amount)
+    Dollar.new(amount)
+  end
+
+  def self.franc(amount)
+    Franc.new(amount)
+  end
+
   def equal?(money)
     self.amount == money.amount && self.class == money.class
   end
