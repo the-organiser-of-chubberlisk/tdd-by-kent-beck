2074e29b544916c584aed9a1aa43fbb0a7e904c3
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-03 20:06:46 +0000

    part_i: push functionality to money superclass (#1)

--- part_i_the_money_example/money.rb
+++ part_i_the_money_example/money.rb
@@ -1,19 +1,28 @@
 class Money
-  attr_reader :amount
+  attr_reader :amount,:currency
+
+  def initialize(amount, currency)
+    @amount = amount
+    @currency = currency
+  end
 
   def self.dollar(amount)
-    Dollar.new(amount)
+    Money.new(amount, "USD")
   end
 
   def self.franc(amount)
-    Franc.new(amount)
+    Money.new(amount, "CHF")
   end
 
   def equal?(money)
-    self.amount == money.amount && self.class == money.class
+    self.amount == money.amount && self.currency == money.currency
   end
 
   def ==(money)
-    self.amount == money.amount && self.class == money.class
+    self.amount == money.amount && self.currency == money.currency
+  end
+
+  def times(multiplier)
+    Money.new(@amount * multiplier, @currency)
   end
 end
