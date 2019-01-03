9615bf1ba49faae711056d38d5801e76fab68aac
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-03 20:07:18 +0000

    part_i: update tests with change of only money class (#1)

--- part_i_the_money_example/test.rb
+++ part_i_the_money_example/test.rb
@@ -1,6 +1,4 @@
 require 'minitest/autorun'
-require_relative './dollar'
-require_relative './franc'
 require_relative './money'
 
 class Test < Minitest::Test
@@ -13,14 +11,11 @@ class Test < Minitest::Test
   def test_equality
     assert Money.dollar(5).equal?(Money.dollar(5))
     refute Money.dollar(5).equal?(Money.dollar(6))
-    assert Money.franc(5).equal?(Money.franc(5))
-    refute Money.franc(5).equal?(Money.franc(6))
     refute Money.franc(5).equal?(Money.dollar(5))
   end
 
-  def test_franc_multiplication
-    five = Money.franc(5)
-    assert_equal Money.franc(10), five.times(2)
-    assert_equal Money.franc(15), five.times(3)
+  def test_currency
+    assert_equal "USD", Money.dollar(1).currency
+    assert_equal "CHF", Money.franc(1).currency
   end
 end
