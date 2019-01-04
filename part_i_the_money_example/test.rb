1ab73ff7eb6eacff2aecc345aa645f2cfab10a1e
Wen Ting Wang <chubberlisk@outlook.com>
2019-01-04 17:34:22 +0000

    part_i: add tests for mixed currency addition (#1)

--- part_i_the_money_example/test.rb
+++ part_i_the_money_example/test.rb
@@ -1,5 +1,7 @@
 require 'minitest/autorun'
 require_relative './money'
+require_relative './bank'
+require_relative './sum'
 
 class Test < Minitest::Test
   def test_multiplication
@@ -20,7 +22,71 @@ class Test < Minitest::Test
   end
 
   def test_simple_addition
-    sum = Money.dollar(5).plus(Money.dollar(5))
-    assert_equal Money.dollar(10), sum
+    five = Money.dollar(5)
+    sum = five.plus(five)
+    bank = Bank.new
+    reduced = bank.reduce(sum, "USD")
+    assert_equal Money.dollar(10), reduced
+  end
+
+  def test_plus_returns_sum
+    five = Money.dollar(5)
+    result = five.plus(five)
+    sum = result
+    assert_equal five, sum.augend
+    assert_equal five, sum.addend
+  end
+
+  def test_reduce_sum
+    sum = Sum.new(Money.dollar(3), Money.dollar(4))
+    bank = Bank.new
+    result = bank.reduce(sum, "USD")
+    assert_equal Money.dollar(7), result
+  end
+
+  def test_reduce_money
+    bank = Bank.new
+    result = bank.reduce(Money.dollar(1), "USD")
+    assert_equal Money.dollar(1), result
+  end
+
+  def test_reduce_money_different_currency
+    bank = Bank.new
+    bank.add_rate("CHF", "USD", 2)
+    result = bank.reduce(Money.franc(2), "USD")
+    assert_equal Money.dollar(1), result
+  end
+
+  def test_identity_rate
+    assert_equal 1, Bank.new.rate("USD", "USD")
+  end
+
+  def test_mixed_addition
+    five_bucks = Money.dollar(5)
+    ten_francs = Money.franc(10)
+    bank = Bank.new
+    bank.add_rate("CHF", "USD", 2)
+    result = bank.reduce(five_bucks.plus(ten_francs), "USD")
+    assert_equal Money.dollar(10), result
+  end
+
+  def test_sum_plus_money
+    five_bucks = Money.dollar(5)
+    ten_francs = Money.franc(10)
+    bank = Bank.new
+    bank.add_rate("CHF", "USD", 2)
+    sum = Sum.new(five_bucks, ten_francs).plus(five_bucks)
+    result = bank.reduce(sum, "USD")
+    assert_equal Money.dollar(15), result
+  end
+
+  def test_sum_times
+    five_bucks = Money.dollar(5)
+    ten_francs = Money.franc(10)
+    bank = Bank.new
+    bank.add_rate("CHF", "USD", 2)
+    sum = Sum.new(five_bucks, ten_francs).times(2)
+    result = bank.reduce(sum, "USD")
+    assert_equal Money.dollar(20), result
   end
 end
